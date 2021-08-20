24d30e0f8df752f404fffc98aba09fd3edf5a1f7
Kyle Huynh <kyle.huynh205@gmail.com>
2021-08-19 20:33:10 -0400

    linked simple search block config with front end

--- src/Form/SimpleSearchForm.php
+++ src/Form/SimpleSearchForm.php
@@ -7,29 +7,38 @@ use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use \Drupal\Core\Url;
 
-class SimpleSearchForm extends FormBase {
+class SimpleSearchForm extends FormBase
+{
   /**
    * {@inheritdoc}
    */
-  public function getFormId() {
+  public function getFormId()
+  {
     return 'simple_search_form';
   }
 
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
+    $block = \Drupal\block\Entity\Block::load("search");
 
+    if ($block) {
+      $settings = $block->get('settings');
+      $view_machine_name = $settings['search_view_machine_name'];
+
+    }
     $form['search-textfield'] = array(
       '#type' => 'textfield',
-      '#title' => t('Search:'),
-        '#attributes' => ['placeholder' => 'Search the collections']
+      '#title' => (!empty($settings['search_textfield_label']) ? $settings['search_textfield_label'] : ''),
+      '#attributes' => ['placeholder' => 'Search the collections']
     );
 
     $form['actions']['#type'] = 'actions';
     $form['actions']['submit'] = array(
       '#type' => 'submit',
-      '#value' => $this->t('SEARCH'),
+      '#value' => (!empty($settings['search_submit_label']) ? $settings['search_submit_label'] : ''),
       '#button_type' => 'primary',
     );
     return $form;
@@ -38,14 +47,21 @@ class SimpleSearchForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
+    $block = \Drupal\block\Entity\Block::load("search");
+    if ($block) {
+      $settings = $block->get('settings');
+      $view_machine_name = $settings['search_view_machine_name'];
+    }
+
     global $base_url;
-    $url = Url::fromRoute('view.advanced_search.page_1',  [
+    $url = Url::fromRoute($view_machine_name, [
       'a[0][f]' => 'title',
       'a[0][i]' => 'IS',
       'a[0][v]' => $form_state->getValues()['search-textfield'],
 
-      ]);
+    ]);
     $form_state->setRedirectUrl($url);
 
   }
--- src/Plugin/Block/SimpleSearchBlock.php
+++ src/Plugin/Block/SimpleSearchBlock.php
@@ -27,18 +27,35 @@ class SimpleSearchBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function blockForm($form, FormStateInterface $form_state) {
-    $form['search_textfield'] = [
+    $form['advanced_search_view_textfield'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Search Keyword Textfield Label'),
-      '#default_value' => $this->configuration['search_textfield'],
+      '#title' => $this->t('Advanced Search View Machine Name:'),
+      '#default_value' => $this->configuration['search_view_machine_name'],
       '#maxlength' => 255,
       '#size' => 64,
       '#weight' => '1',
     ];
+    $form['search_textfield'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Search Keyword Textfield Label:'),
+      '#default_value' => $this->configuration['search_textfield_label'],
+      '#maxlength' => 255,
+      '#size' => 64,
+      '#weight' => '1',
+    ];
+    $form['search_placeholder_textfield'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Search Keyword Textfield Placeholder:'),
+      '#default_value' => $this->configuration['search_placeholder'],
+      '#maxlength' => 255,
+      '#size' => 64,
+      '#weight' => '1',
+    ];
+
     $form['search_submit'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Search Button Label'),
-      '#default_value' => $this->configuration['search_textfield'],
+      '#title' => $this->t('Search Button Label:'),
+      '#default_value' => $this->configuration['search_submit_label'],
       '#maxlength' => 255,
       '#size' => 64,
       '#weight' => '1',
@@ -51,8 +68,10 @@ class SimpleSearchBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
-    $this->configuration['search_textfield'] = $form_state->getValue('search_textfield');
-    $this->configuration['search_submit'] = $form_state->getValue('search_submit');
+    $this->configuration['search_view_machine_name'] = $form_state->getValue('advanced_search_view_textfield');
+    $this->configuration['search_textfield_label'] = $form_state->getValue('search_textfield');
+    $this->configuration['search_placeholder'] = $form_state->getValue('search_placeholder_textfield');
+    $this->configuration['search_submit_label'] = $form_state->getValue('search_submit');
   }
 
   /**
