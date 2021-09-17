40b8719d87ad6eef4f764a0754555783308fedfb
Kyle Huynh <kyle.huynh205@gmail.com>
2021-09-17 09:21:16 -0400

    Fix the issue of unable to refresh Facets block when Ajax is enabled.

--- js/facets/facets-views-ajax.js
+++ js/facets/facets-views-ajax.js
@@ -42,7 +42,7 @@
     // Replace filter, pager, summary, and facet blocks.
     var blocks = {};
     $(
-      ".block[class*='block-plugin-id--islandora-advanced-search-result-pager'], .block[class*='block-plugin-id--views-exposed-filter-block'], .block[class*='block-plugin-id--facet']"
+      ".block[class*='block-plugin-id--islandora-advanced-search-result-pager'], .block[class*='block-plugin-id--views-exposed-filter-block'], .block[class*='block-facets']"
     ).each(function () {
       var id = $(this).attr("id");
       var block_id = id
@@ -119,7 +119,7 @@
       // Attach behavior to pager, summary, facet links.
       $("[data-drupal-pager-id], [data-drupal-facets-summary-id], [data-drupal-facet-id]")
         .once()
-        .find("a:not(.facets-soft-limit-link)")
+        .find("a:not(.facet-item)")
         .click(function (e) {
           // Let ctrl/cmd click open in a new window.
           if (e.shiftKey || e.ctrlKey || e.metaKey) {
