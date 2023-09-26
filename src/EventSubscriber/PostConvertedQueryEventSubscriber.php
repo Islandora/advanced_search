6123df49a2038b71d3c3129fbcea53981703be4c
Rosie Le Faive <lefaive@gmail.com>
2023-09-26 12:51:02 -0300

    Add event subscriber.

---
+++ src/EventSubscriber/PostConvertedQueryEventSubscriber.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\advanced_search\EventSubscriber;
+
+use Drupal\advanced_search\AdvancedSearchQuery;
+use Drupal\search_api_solr\Event\SearchAPISolrEvents;
+use Drupal\search_api_solr\Event\PostConvertedQueryEvent;
+
+class PostConvertedQueryEventSubscriber extends EventSubscriberInterface() {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[SearchAPISolrEvents::POST_CONVERT_QUERY][] = ['alter'];
+
+    return $events;
+
+  }
+
+  /**
+   * Alter the query.
+   */
+  public function alter(PostConvertedQueryEvent $event) {
+    $search_api_query = $event->getSearchApiQuery();
+    $solarium_query = $event->getSolariumQuery();
+    $advanced_search_query = new AdvancedSearchQuery();
+    $advanced_search_query->alterQuery(\Drupal::request(), $solarium_query, $search_api_query);
+  }
+
+}
